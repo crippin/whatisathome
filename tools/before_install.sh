0203092057f562e66fef8a39741e96675a4c726f
Istvan Miklos <miklos.istvan2@gmail.com>
2019-07-07 20:35:12 +0200

    Imporove travis jobs

--- tools/before_install.sh
+++ tools/before_install.sh
@@ -2,6 +2,8 @@
 
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 
+git fetch --all
+
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
     pip install six
@@ -12,22 +14,12 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
     brew install --HEAD libimobiledevice
     brew install ideviceinstaller
     brew install ios-deploy
+    gem install cocoapods -v '1.6.0'
 
 fi
 
 git clone https://github.com/flutter/flutter.git -b $FLUTTER_VERSION
 
-case $TRAVIS_BRANCH in
-    flutter_web)
-        ./flutter/bin/flutter channel dev
-        ./flutter/bin/flutter pub global activate webdev
-        ;;
-    flutter_desktop)
-        ./flutter/bin/flutter channel master
-        ./flutter/bin/flutter upgrade
-        ;;
-    *)
-        ;;
-esac
-
+./flutter/bin/flutter pub global activate webdev
+./flutter/bin/flutter upgrade
 ./flutter/bin/flutter pub get
--- tools/travis_script.sh
+++ tools/travis_script.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-case $TRAVIS_BRANCH in
-    flutter_web)
-        ./flutter/bin/flutter packages pub global run webdev build -r
-        ;;
-    flutter_desktop)
-        git submodule update
-        cp -R flutter-desktop-embedding/example/linux .
-        cp -R flutter-desktop-embedding/example/fonts .
-        if [ $TRAVIS_OS_NAME = 'osx' ]; then
-            ./flutter/bin/flutter -v build macosx
-        else
-            ./flutter/bin/flutter -v build linux
-        fi
-        ;;
-    *)
+case $FLUTTER_VERSION in
+    stable) #Android/ios
+        git checkout master
         if [ $TRAVIS_OS_NAME = 'osx' ]; then
             ./flutter/bin/flutter -v build ios --no-codesign
         else
             ./flutter/bin/flutter -v build apk
         fi
-        ./flutter/bin/flutter -v test
+        ;;
+    dev) # Web (html,js)
+        git checkout flutter_web
+        ./flutter/bin/flutter packages pub global run webdev build -r
+        ;;
+    master) # Desktop
+        git checkout flutter_desktop
+        git submodule update
+        cp -R flutter-desktop-embedding/example/linux .
+        cp -R flutter-desktop-embedding/example/macos .
+        cp -R flutter-desktop-embedding/example/fonts .
+        if [ $TRAVIS_OS_NAME = 'osx' ]; then
+            ./flutter/bin/flutter -v build macos
+        else
+            ./flutter/bin/flutter -v build linux
+        fi
         ;;
 esac
